d9b29eea5ea86ecc3df0306997b1435d47f51250
kumarindenmark <kumarindenmark@gmail.com>
2018-06-02 22:09:58 +0200

    add SASS setup

--- src/index.js
+++ src/index.js
@@ -1,13 +1,4 @@
  import _ from 'lodash';
+ import './styles/main.scss';
 
-  function component() {
-    var element = document.createElement('div');
-
-   // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-
-    return element;
-  }
-
-  document.body.appendChild(component());
+ console.log('app is running!!!')
